4de750489e7b366cc2970ee25da6dc35b6984ede
Roland <cook.rolandcook.roland@gmail.com>
2021-11-06 16:44:45 -0400

    Finished functionality
    Finished routes and updated CSS on contact details page

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import user from "../images/user.png"
+import './contactDetail.css'
 
 const ContactDetail = (props)=>{
     const {name,email}=props.location.state.contacts
 return (
-    <div className="main">
+    <div className="main mg1">
         <div className="ui card centered">
             <div className="image">
                 <img src={user} alt={user}></img>
@@ -15,7 +16,7 @@ return (
                 <div className="description">{email}</div>
             </div>
         </div>
-        <div className="center-div">
+        <div className="center-div center">
            <Link to ="/"> <button className="ui button blue center" >Back to Contact List</button>
            </Link>
         </div>
