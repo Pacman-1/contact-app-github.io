e2f14d242fa027650b1c2b36c83987ec11b68599
Roland <cook.rolandcook.roland@gmail.com>
2021-12-05 15:38:48 -0500

    Update contactList.js

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ContactCard from './ContactCard'
+import ContactCard from './contactCard'
 import { Link } from 'react-router-dom'
 
 const Contactlist = (props) =>{
